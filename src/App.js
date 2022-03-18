ce2cca68cc88a2c239a81c70d37ed435dadd0308
Leshan <leshan0627@gmail.com>
2022-03-18 17:20:57 -0400

    docs: Implement Body.js and body.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./components/Nav.js";
 import Header from "./components/Header.js";
+import Body from "./components/Body.js"
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../style/Body.css";
+
+export default function Body() {
+  return (
+    <section className="body-div_1">
+      <div className="body-div_2">
+        <h2 className="h2"> Lorum Ipsum</h2>
+        <button className="button-body"></button>  
+      </div>
+      <div className="body-process">
+        <div className="body-process_text">
+          <h2>Lorem epsum</h2>
+        </div>
+        <div className="body-process_image">
+        </div>
+      </div>
+    </section>
+  );
+}
