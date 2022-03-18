673d7e7a710540d6ad4999ecd6f57aeaada4e4b4
Leshan <leshan0627@gmail.com>
2022-03-18 16:21:26 -0400

    docs: Implement Header.js and Header.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Nav from "./components/Nav.js"
+import "./App.css";
+import Nav from "./components/Nav.js";
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <div className="App">
-     <Nav />
+      <Nav />
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
+import "../style/Header.css"
 
 export default function Header() {
  return (
-   
+  <section className="header-div_1">
+    <div className="header-div_2">
+      <div className="header-div_3">
+       <h1>Save Money</h1>
+       <h1>Save The Planet</h1>
+       <h1>Save Small Business</h1>
+       </div>
+     <button className="button">Downlaod App</button>
+    </div>
+    <div className="header-div_4">
+
+    </div>
+  </section>
  ) 
 }
