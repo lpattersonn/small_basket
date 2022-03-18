6a43c013845f7b0fa0e7531a9234d82e2589d49d
Leshan <leshan0627@gmail.com>
2022-03-18 18:08:53 -0400

    fix: Connect user and grocery side pages

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,14 +6,15 @@ export default function Body(props) {
     <section className="body-div_1">
       <div className="body-div_2">
         <h2 className="h2"> Lorum Ipsum</h2>
-        <button className="button-body"></button>  
+        <button className="button-body">
+          <a href="/groceryside">Click Here</a>
+        </button>
       </div>
       <div className="body-process">
         <div className="body-process_text">
           <h2>Lorem epsum</h2>
         </div>
-        <div className="body-process_image">
-        </div>
+        <div className="body-process_image"></div>
       </div>
     </section>
   );
