a88e9d919e0af6bfe4e7d0b0bf3d3c71c3265290
Leshan <leshan0627@gmail.com>
2022-03-18 15:58:33 -0400

    docs: Implement Nav.js and Nav.css

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style/Nav.css";
+
+export default function Nav() {
+  return (
+    <section className="nav">
+      <div className="nav-div_1">
+        <h1>Logo</h1>
+      </div>
+      <div className="nav-div_2">
+        <ul>
+          <li>Login</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
