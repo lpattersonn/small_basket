5658fc439d689c089ec6f121b81d0bda7f48ff9d
caroline <carzhang96@gmail.com>
2022-03-18 17:22:49 -0400

    docs Implement grocer-side/Header.js and grocer-side/Nav.js

---
+++ src/components/grocer-side/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../style/Header.css"
+
+export default function Header() {
+ return (
+  <section className="header-div_1">
+    <div className="header-div_2">
+      <div className="header-div_3">
+       <h1>Are you a small grocer?</h1>
+       <p>We want to work with small businesses like you to reduce food waste and help you reach your customer base more effectively.</p>
+       </div>
+     <button className="button">Partner with us</button>
+    </div>
+    <div className="header-div_4">
+
+    </div>
+  </section>
+ ) 
+}
