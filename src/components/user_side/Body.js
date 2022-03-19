5cbd31ab95dc5856eb3e4f3907fe5e96c06d5295
SergeySharipov <sharipov14@gmail.com>
2022-03-19 19:10:47 -0400

    docs: Update user side body

--- src/components/user_side/Body.js
+++ src/components/user_side/Body.js
@@ -2,42 +2,54 @@ import React from "react";
 import "../../style/user_side/Body.css";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { FaMoneyBillAlt } from "react-icons/fa";
+import emoji from "../../images/how we roll emoji.png";
+import browseItems from "../../images/browse items.png";
+import checkout from "../../images/checkout.png";
+import pickUpOrder from "../../images/pick-up order.png";
+import icQuick from "../../images/ic_quick.png";
+import icMask from "../../images/ic_mask.png";
+import icHelp from "../../images/ic_help.png";
 
 export default function Body(props) {
   return (
     <section className="user_side-body-div_1">
       <div className="user_side-body-div_2">
-        <h2 className="user_side-body-h2"> Lorum Ipsum</h2>
-        <button className="user_side-body-button">
-          <a href="/grocerside">Click Here</a>
-        </button>
+        <h1 className="user_side-body-h1">How we roll</h1>
+        <img className="user_side-body-emoji" src={emoji} alt="" />
       </div>
       <div className="user_side-body-process">
-        <div className="user_side-body-process_text">
-          <h2>Step by step process</h2>
+        <div>
+          <img src={browseItems} alt="" />
+          <p className="user_side-body-process_text">Browse through items at a small grocer near you using our app.</p>
         </div>
         <div className="user_side-body-process_image"></div>
       </div>
       <div className="user_side-body-process">
-        <div className="user_side-body-whoweare_image"></div>
-        <div className="user_side-body-whoweare_text">
-          <h2>Who we are</h2>
+        <div>
+          <img src={checkout} alt="" />
+          <p className="user_side-body-process_text">Browse through items at a small grocer near you using our app.</p>
         </div>
-      </div>
-      <div className="user_side-body-stats">
-        <div className="user_side-body-stats_food">
-          <FaRegTrashAlt className="user_side-body-faicon" />
-          <h2>food waste</h2>
-        </div>
-        <div className="user_side-body-stats_money">
-          <FaMoneyBillAlt className="user_side-body-faicon" />
-          <h2>save money</h2>
-        </div>
+        <div className="user_side-body-process_image"></div>
       </div>
       <div className="user_side-body-process">
-        <div className="user_side-body-whoweare_image"></div>
-        <div className="user_side-body-whoweare_text">
-          <h2>Who we serve</h2>
+        <div>
+          <img src={pickUpOrder} alt="" />
+          <p className="user_side-body-process_text">Browse through items at a small grocer near you using our app.</p>
+        </div>
+        <div className="user_side-body-process_image"></div>
+      </div>
+      <div className="user_side-body-stats">
+        <div className="user_side-body-stats_item">
+          <img src={icQuick} alt="" />
+          <h2>Quick and<br/>convenient</h2>
+        </div>
+        <div className="user_side-body-stats_item">
+          <img src={icMask} alt="" />
+          <h2>Distanced<br/>Shopping</h2>
+        </div>
+        <div className="user_side-body-stats_item">
+          <img src={icHelp} alt="" />
+          <h2>Help small<br/>businesses in their<br/>time of need</h2>
         </div>
       </div>
       <div className="user_side-body-downlaod">
