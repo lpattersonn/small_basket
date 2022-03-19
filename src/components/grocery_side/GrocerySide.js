88fba9d93d1eb19d7bd2d31ba3726b5afb257303
SergeySharipov <sharipov14@gmail.com>
2022-03-19 13:17:08 -0400

    docs: Implement grocery side Body.js and Body.css

--- src/components/grocery_side/GrocerySide.js
+++ src/components/grocery_side/GrocerySide.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "../Nav.js";
 import Header from "./Header.js";
+import Body from "./Body.js";
 
 export default function GrocerySide(props) {
   return (
     <div className="grocery_side">
       <Nav />
       <Header />
+      <Body />
     </div>
   );
 }
