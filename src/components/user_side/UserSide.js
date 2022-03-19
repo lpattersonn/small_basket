a21def05bf056c909d549267133e3af0eb4a4b39
SergeySharipov <sharipov14@gmail.com>
2022-03-19 17:10:36 -0400

    docs: Implement user side nav and header design

--- src/components/user_side/UserSide.js
+++ src/components/user_side/UserSide.js
@@ -2,12 +2,16 @@ import Nav from "./Nav.js";
 import Header from "./Header.js";
 import Body from "./Body.js";
 import Footer from "./Footer.js";
+import background from "../../images/banner background.svg";
+import "../../style/user_side/UserSide.css"
 
 function UserSide(props) {
   return (
     <div className="user_side">
-      <Nav />
-      <Header />
+      <div className="user_side-top" style={{ backgroundImage: `url(${background})` }}>
+        <Nav />
+        <Header />
+      </div>
       <Body />
       <Footer />
     </div>
