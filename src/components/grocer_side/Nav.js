1907a35f9750f654d2e6c3e544e2683b88c1aa4c
caroline <carzhang96@gmail.com>
2022-03-19 15:21:09 -0400

    style: Update Nav.js and Nav.css

--- src/components/grocer_side/Nav.js
+++ src/components/grocer_side/Nav.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "../../style/grocer_side/Nav.css"
+import logo from "../../images/logo.png"
 
 export default function Nav(props) {
   return (
     <section className="grocer_side-nav">
       <div className="grocer_side-nav-div_1">
-        <h1>Logo</h1>
+        <img src={logo} alt="small basket logo"/>
       </div>
       <div className="grocer_side-nav-div_2">
         <ul>
-          <li>Login</li>
+          <li>About</li>
+          <li>Download</li>
+          <li>Partner with us</li>
+          <li>Support</li>
         </ul>
       </div>
     </section>
