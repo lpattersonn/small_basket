ef3648f60983310936850fc93218119969c35320
caroline <carzhang96@gmail.com>
2022-03-19 23:04:18 -0400

    fix: Fix grocer side Nav spacing

--- src/components/grocer_side/Nav.js
+++ src/components/grocer_side/Nav.js
@@ -6,7 +6,7 @@ export default function Nav(props) {
   return (
     <section className="grocer_side-nav">
       <div className="grocer_side-nav-div_1">
-        <img src={logo} alt="small basket logo"/>
+        <img className="grocer_side-nav-logo"src={logo} alt="small basket logo"/>
       </div>
       <div className="grocer_side-nav-div_2">
         <ul>
